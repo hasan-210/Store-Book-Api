92ab45388a711f5cbc6d6689bdf92088f0030f0c
hasan-1 <hasanjasser551@gmail.com>
2025-01-25 13:41:16 +0200

    upload image

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const logger = require('./middleware/logger');
 const {notFound,errorHandler} = require('./middleware/error')
 require('dotenv').config();
 const connectToDb  = require('./config/db');
-
+const path = require("path") 
 // conecction db
 connectToDb();
 
 //init
 const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname,"images")));
 
 // Apply Middlewares
 app.use(express.json()); 
@@ -21,6 +24,7 @@ app.use('/api/books',require('./routes/books'));
 app.use('/api/authors',require('./routes/authors'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/users',require('./routes/users'));
+app.use('/api/upload',require('./routes/upload'));
 app.use('/password',require('./routes/password'));
 
 
